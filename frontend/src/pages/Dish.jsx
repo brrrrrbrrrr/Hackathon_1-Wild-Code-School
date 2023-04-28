4ae240a40169a08028da7611f016baae82932a2c
Mehdi Charef <me-charef@hotmail.fr>
2023-04-28 10:08:27 +0200

    finished individual dish page

--- frontend/src/pages/Dish.jsx
+++ frontend/src/pages/Dish.jsx
@@ -1,11 +1,40 @@
+// import React, { useEffect, useState } from "react";
+// import axios from "axios";
+// import { useParams } from "react-router-dom";
+// import DishPageItem from "../components/dishPageItem/DishPageItem";
+// import "./Dish.css";
+
+// function Dish() {
+//   const { id } = useParams();
+//   const [dish, setDish] = useState([]);
+//   useEffect(() => {
+//     axios
+//       .get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+//       .then((response) => {
+//         console.warn(response.data.meals[0]);
+//         setDish(response.data.meals[0]);
+//       })
+//       .catch((err) => console.error(err));
+//   }, []);
+//   return (
+//     <div>
+//       <DishPageItem content={dish} />
+//     </div>
+//   );
+// }
+
+// export default Dish;
+
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import DishPageItem from "../components/dishPageItem/DishPageItem";
+import "./Dish.css";
 
 function Dish() {
   const { id } = useParams();
   const [dish, setDish] = useState([]);
+
   useEffect(() => {
     axios
       .get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
@@ -15,8 +44,12 @@ function Dish() {
       })
       .catch((err) => console.error(err));
   }, []);
+
   return (
     <div>
+      <div className="loader-container">
+        <div className="loader" />
+      </div>
       <DishPageItem content={dish} />
     </div>
   );
